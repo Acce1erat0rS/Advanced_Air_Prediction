69188011e6b9109b2e7e702d8fd2b34a0b7437c4
Acce1erat0rS <i@liutianyu.cn>
2017-10-15 14:58:34 +0800

    Reconstructing codes.

--- Baseline_3City_test.py
+++ Baseline_3City_test.py
@@ -1,13 +1,10 @@
 import numpy as np
-import tensorflow as tf
-from tensorflow.contrib import rnn
+import keras
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Activation
+from keras.layers import Embedding
+from keras.layers import LSTM
 import random
-
-
-timestep_size = 71                # Hours of looking ahead
-output_parameters = 3   # Number of predicting parameters
-num_stations = 3        # Number of monitoring stations
-
 
 data_dir = "/home/spica/mnt_device/aqi/dev_data/timesub/"
 def process(x):
@@ -21,10 +18,6 @@ split = 200
 leap = 6
 length = 16
 
-lr = 0.0001
-hidden_size = 256
-layer_num = 3
-
 
 # from UNIX time 1395691200
 # to UNIX time 1448564400
@@ -33,6 +26,7 @@ data = []
 target_set = []
 
 # defines how many hours is used to predict
+hop = 71
 
 print("Processing target set")
 start = 1395691200;
@@ -76,6 +70,15 @@ set = np.array(X[1920:])
 target = np.array(y[1920:])
 val_set = np.array(X[:1920])
 val_target = np.array(y[:1920])
+# np.append(target, 32)
+# ----------------------remain to be changed---------------------
+
+# target.size = (set.size()[0], 1)
+print "the dataset:\n"
+print set
+print "the target\n"
+print target
+
 
 # DENSE VER
 '''
@@ -96,79 +99,28 @@ model.fit(set, target, nb_epoch=100, batch_size=32)
 '''
 
 
-sess = tf.InteractiveSession()
-batch_size = tf.placeholder(tf.int32)
-_X = tf.placeholder(tf.float32, [None, timestep_size, 36])     # TODO change this to the divided ver
-y = tf.placeholder(tf.float32, [None, 3])
-keep_prob = tf.placeholder(tf.float32)
+# LSTM VER
+model = Sequential()
+# model.add(Embedding(1000, 256, input_length=322))
+model.add(LSTM(batch_input_shape=(384,71, 36),
+	return_sequences=True,
+	#stateful=True,
+	output_dim=256,
+	activation='sigmoid',
+	inner_activation='hard_sigmoid'))
+model.add(Activation("relu"))
+model.add(LSTM( output_dim = 256, activation='sigmoid', inner_activation='hard_sigmoid', name = "LSTM-2"))
+model.add(Activation("relu"))
+# model.add(LSTM((384,256),output_dim=128, activation='sigmoid', inner_activation='hard_sigmoid'))
+# model.add(Activation("relu"))
+model.add(Dense(3))
 
-# --------------------------------------------
-#             Construct LSTM cells
-# --------------------------------------------
+model.compile(loss='mae',
+              optimizer='Adagrad',
+              metrics=['accuracy'])
+keras.optimizers.Adagrad(lr=0.01, epsilon=1e-08)
 
-lstm_cell = rnn.BasicLSTMCell(num_units=hidden_size,
-                              forget_bias=1.0,
-                              state_is_tuple=True)
+model.fit(set, target, batch_size=384, nb_epoch=1000, validation_data=(val_set, val_target))
+# score = model.evaluate(testset, testtarget, batch_size=16)
 
-lstm_cell = rnn.DropoutWrapper(cell=lstm_cell,
-                               input_keep_prob=1.0,
-                               output_keep_prob=keep_prob)
-
-mlstm_cell = rnn.MultiRNNCell([lstm_cell] * layer_num, state_is_tuple=True)
-
-init_state = mlstm_cell.zero_state(batch_size, dtype=tf.float32)
-
-# ** 当 time_major==False 时， outputs.shape = [batch_size, timestep_size, hidden_size]
-# ** 所以，可以取 h_state = outputs[:, -1, :] 作为最后输出
-# ** state.shape = [layer_num, 2, batch_size, hidden_size],
-# ** 或者，可以取 h_state = state[-1][1] 作为最后输出
-# ** 最后输出维度是 [batch_size, hidden_size]
-outputs, state = tf.nn.dynamic_rnn(mlstm_cell,
-                                   inputs=X,
-                                   initial_state=init_state,
-                                   time_major=False)
-h_state = outputs[:, -1, :]  # 或者 h_state = state[-1][1]
-
-# *************** 为了更好的理解 LSTM 工作原理，我们把上面 步骤6 中的函数自己来实现 ***************
-# 通过查看文档你会发现， RNNCell 都提供了一个 __call__()函数（见最后附），我们可以用它来展开实现LSTM按时间步迭代。
-# **步骤6：方法二，按时间步展开计算
-# outputs = list()
-# state = init_state
-# with tf.variable_scope('RNN'):
-#     for timestep in range(timestep_size):
-#         if timestep > 0:
-#             tf.get_variable_scope().reuse_variables()
-#         # 这里的state保存了每一层 LSTM 的状态
-#         (cell_output, state) = mlstm_cell(X[:, timestep, :], state)
-#         outputs.append(cell_output)
-# h_state = outputs[-1]
-
-
-# --------------------------------------------
-#    Convert LSTM output to tensor of three
-# --------------------------------------------
-W = tf.Variable(tf.truncated_normal([hidden_size, output_parameters],
-                                    stddev=0.1),
-                dtype=tf.float32)
-bias = tf.Variable(tf.constant(0.1,shape=[output_parameters]),
-                   dtype=tf.float32)
-y_pre = tf.matmul(h_state, W) + bias
-
-cross_entropy = -tf.reduce_mean(y * tf.log(y_pre))
-train_op = tf.train.AdamOptimizer(lr).minimize(cross_entropy)
-
-correct_prediction = tf.equal(tf.argmax(y_pre,1), tf.argmax(y,1))
-accuracy = tf.reduce_mean(tf.cast(correct_prediction, "float"))
-
-
-sess.run(tf.global_variables_initializer())
-
-for i in range(2000):
-    _batch_size = 384
-    batch = random.randint(5, 36)
-    sess.run(train_op,
-             feed_dict={_X:data[batch*_batch_size:(batch+1)*_batch_size, :, :],
-                        y: data[batch*_batch_size:(batch+1)*_batch_size, :],
-                        keep_prob: 1.0,
-                        batch_size: _batch_size})
 
