ee1dc589dbac126501c8125927a24aa859ee6262
Spica Liu <i@liutianyu.cn>
2017-10-15 12:59:09 +0800

    Development Baseline finished

--- Baseline_3City_test.py
+++ Baseline_3City_test.py
@@ -6,7 +6,7 @@ from keras.layers import Embedding
 from keras.layers import LSTM
 import random
 
-data_dir = "/home/spica/mnt_device/aqi/dev_data/"
+data_dir = "/home/spica/mnt_device/aqi/dev_data/timesub/"
 def process(x):
     if x == '?':
         return 0.0
@@ -18,68 +18,58 @@ split = 200
 leap = 6
 length = 16
 
+
+# from UNIX time 1395691200
+# to UNIX time 1448564400
 # Read from the file of the training set
 data = []
-
-print("Processing target set")
-f = open(data_dir+"Beijing", 'r')
-l = len(f.readlines())
-ls = []
-f.close()
-buff = []
 target_set = []
 
-for line in range(96,14688):
-    f1 = open(data_dir+"Beijing", "r")
-#    f2 = open("Tianjin","r")
-#    f3 = open("Huludao","r")
-    ls1 = f1.readlines()[line].split("#")
-#    ls2 = f2.readlines()[line].split("#")
-#    ls3 = f3.readlines()[line].split("#")
-#    buff = []
-#    buff.append(map(float,ls1[7:10]))
-#    buff.append(map(float,ls2[7:10]))
-#    buff.append(map(float,ls3[7:10]))
-#    target_set.append(buff)
-    f1.close()
-    target_set.append(map(float,ls1[7:10]))
-#    f2.close()
-#    f3.close()
+# defines how many hours is used to predict
+hop = 71
 
-print("Processing trainingset")
-for i in range(14592):
-    f1 = open(data_dir+"Beijing", "r")
-    f2 = open(data_dir+"Tianjin","r")
-    f3 = open(data_dir+"Huludao","r")
+print("Processing target set")
+start = 1395691200;
+end = 1448564400;
+cur_start = start;
+cur_end = start+hop*3600;
+
+while(cur_end<end-(120+288)*3600):
     buff = []
-    for i in range(71):
-	buff.append([])
-    count = 0
-    for line in f1.readlines()[i:i+71]:
-        ls = line.split('#')
-        buff[count].extend(map(float,ls[4:16]))
-        count = count+1
-    f1.close()
-    count = 0
-    for line in f2.readlines()[i:i+71]:
-        ls = line.split('#')
-        buff[count].extend(map(float,ls[4:16]))
-        count = count+1
-    f2.close()
-    count = 0
-    for line in f3.readlines()[i:i+71]:
-        ls = line.split('#')
-        buff[count].extend(map(float,ls[4:16]))
-        count = count+1
-    f3.close()
+    for i in range(hop):
+        hour = []
+        f1 = open(data_dir+(str)(cur_start+i*3600),'rb')
+        for line in f1.readlines():
+            ls = line.split('#')
+            hour = hour+(map(float,ls[4:16]))
+        f1.close()
+        buff.append(hour);
     data.append(buff)
+    f1 = open(data_dir+(str)(cur_start+120*3600),'rb')
+    for line in f1.readlines():
+        ls = line.split("#")
+        target_set.append(map(float,ls[7:10]))
+        break
+    cur_start = cur_start+3600;
+    cur_end = cur_end+3600;
+# s_target = random.shuffle(target_set)
+print(len(target_set))
+np_data = np.asarray(data)
+np_target = np.asarray(target_set)
+print("Target shape :",np_target.shape)
+print("Data shape : :",np_data.shape)
+# training_data = np.hstack((np_data,np_target))
+# np.random.shuffle(training_data)
+# X = training_data[:, :-1]
+#y  = training_data[:, -1]
 
-s_data = random.shuffle(data)
-s_target = random.shuffle(target_set)
-set = np.array(s_data[:12288])
-target = np.array(s_targe[:12288])
-val_set = np.array(s_data[12288:])
-val_target = np.array(s_target[12288:])
+X = np_data
+y = np_target
+
+set = np.array(X[1920:])
+target = np.array(y[1920:])
+val_set = np.array(X[:1920])
+val_target = np.array(y[:1920])
 # np.append(target, 32)
 # ----------------------remain to be changed---------------------
 
@@ -112,12 +102,17 @@ model.fit(set, target, nb_epoch=100, batch_size=32)
 # LSTM VER
 model = Sequential()
 # model.add(Embedding(1000, 256, input_length=322))
-model.add(LSTM(batch_input_shape=(384,71, 36),return_sequences=True, stateful=True, output_dim=256, activation='sigmoid', inner_activation='hard_sigmoid'))
+model.add(LSTM(batch_input_shape=(384,71, 36),
+	return_sequences=True,
+	#stateful=True,
+	output_dim=256, 
+	activation='sigmoid',
+	inner_activation='hard_sigmoid'))
 model.add(Activation("relu"))
 model.add(LSTM( output_dim = 256, activation='sigmoid', inner_activation='hard_sigmoid', name = "LSTM-2"))
 model.add(Activation("relu"))
-#model.add(LSTM((384,256),output_dim=128, activation='sigmoid', inner_activation='hard_sigmoid'))
-#model.add(Activation("relu"))
+# model.add(LSTM((384,256),output_dim=128, activation='sigmoid', inner_activation='hard_sigmoid'))
+# model.add(Activation("relu"))
 model.add(Dense(3))
 
 model.compile(loss='mae',
