55f92cefc3800c2457e04bfc9fe248f554dc8e43
Acce1erat0rS <i@liutianyu.cn>
2017-11-18 14:14:47 +0800

    Reuse?

--- Archetectures.py
+++ Archetectures.py
@@ -8,26 +8,27 @@ import threading
 import os
 
 
-def LSTM(inputs, hidden_size, layer_num, batch_size, keep_prob):
-    lstm_cell = rnn.LSTMCell(num_units=hidden_size,
-                             forget_bias=1.0,
-                             state_is_tuple=True)
-    #                         time_major=False)
+def LSTM(inputs, hidden_size, layer_num, batch_size, keep_prob,init_state):
+    with tf.variable_scope('rnn_cell', reuse=True):
+        lstm_cell = rnn.LSTMCell(num_units=hidden_size,
+                                 forget_bias=1.0,
+                                 state_is_tuple=True)
+        #                         time_major=False)
 
-    lstm_cell = rnn.DropoutWrapper(cell=lstm_cell,
-                                   input_keep_prob=1.0,
-                                   output_keep_prob=keep_prob)
+        lstm_cell = rnn.DropoutWrapper(cell=lstm_cell,
+                                       input_keep_prob=1.0,
+                                       output_keep_prob=keep_prob)
 
-    mlstm_cell = rnn.MultiRNNCell([lstm_cell] * layer_num, state_is_tuple=True)
+        mlstm_cell = rnn.MultiRNNCell([lstm_cell] * layer_num, state_is_tuple=True)
 
-    init_state = mlstm_cell.zero_state(batch_size, dtype=tf.float32)
+        init_state = mlstm_cell.zero_state(batch_size, dtype=tf.float32)
 
-    outputs, state = tf.nn.dynamic_rnn(mlstm_cell,
-                                       inputs=inputs,
-                                       initial_state=init_state)
-    h_state = outputs[:, -1, :]
+        outputs, state = tf.nn.dynamic_rnn(mlstm_cell,
+                                           inputs=inputs,
+                                           initial_state=init_state)
+        h_state = outputs[:, -1, :]
 
-    return h_state
+        return h_state
 
 
 def Affine(input, input_dim, output_dim, trainable=True):
